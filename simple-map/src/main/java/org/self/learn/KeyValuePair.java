32a9b5e49d0e461714ce77c5ee17cf0964675582
Priyan Parida <priyadarshan.parida@citi.com>
2016-02-16 06:53:46 -0500

    Minor Refactors

--- simple-map/src/main/java/org/self/learn/KeyValuePair.java
+++ simple-map/src/main/java/org/self/learn/KeyValuePair.java
@@ -11,14 +11,8 @@ public class KeyValuePair<K, V> {
 	public K getKey() {
 		return key;
 	}
-	public void setKey(K key) {
-		this.key = key;
-	}
 	public V getValue() {
 		return value;
-	}
-	public void setValue(V value) {
-		this.value = value;
 	}
 	
 }
