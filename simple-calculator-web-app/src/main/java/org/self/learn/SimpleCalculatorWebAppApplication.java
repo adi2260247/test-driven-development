660880734e01a041f0d0f4621dad98fc54fd6425
Priyan Parida <priyadarshan.parida@citi.com>
2016-02-20 16:25:34 -0500

    Initial Commit

---
+++ simple-calculator-web-app/src/main/java/org/self/learn/SimpleCalculatorWebAppApplication.java
@@ -0,0 +1,12 @@
+package org.self.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCalculatorWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCalculatorWebAppApplication.class, args);
+	}
+}
